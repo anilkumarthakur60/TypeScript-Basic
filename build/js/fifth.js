532019ac234dec91cab4d515fc2847bf6ab36173
Anil <anilkumarthakur60@gmail.com>
2023-04-05 02:15:25 +0545

    update

--- build/js/fifth.js
+++ build/js/fifth.js
@@ -18,4 +18,38 @@ const assignGraded = updateAssignment(assign1, {
     grade: 95,
 });
 console.log(assignGraded);
+const recordAssignment = (assign) => {
+    return assign;
+};
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+// assignVerified.grade = 99;
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
+const hexColorMap = {
+    red: '#ff0000',
+    green: '#00ff00',
+    blue: '#0000ff'
+};
+console.log(hexColorMap.red);
+const finalGrade = {
+    Sara: "A",
+    Kelly: "B"
+};
+const studentGrades = {
+    Sara: {
+        assign1: 100,
+        assign2: 100
+    },
+    Kelly: {
+        assign1: 90,
+        assign2: 90
+    }
+};
+const score = {
+    studentId: '123',
+    grade: 100
+};
+const preview = {
+    studentId: '123',
+    title: 'Math'
+};
 console.log('------------finish fifthe');
--- src/fifth.ts
+++ src/fifth.ts
@@ -44,5 +44,75 @@ const assignGraded: Assignment = updateAssignment(assign1, {
 console.log(assignGraded);
 
 
+const recordAssignment = (assign: Required<Assignment>): Assignment => {
+
+    return assign
+}
+
+
+const assignVerified: Readonly<Assignment> = {
+    ...assignGraded,
+    verified: true
+}
+
+// assignVerified.grade = 99;
+
+
+recordAssignment({ ...assignGraded, verified: true })
+
+
+
+const hexColorMap: Record<string, string> = {
+    red: '#ff0000',
+    green: '#00ff00',
+    blue: '#0000ff'
+}
+
+console.log(hexColorMap.red);
+
+type Students = "Sara" | "Kelly"
+type LetterGrades = "A" | "B" | "C" | "D" | "U"
+
+const finalGrade: Record<Students, LetterGrades> = {
+    Sara: "A",
+    Kelly: "B"
+}
+
+interface Grades {
+    assign1: number
+    assign2: number
+}
+
+const studentGrades: Record<Students, Grades> = {
+    Sara: {
+        assign1: 100,
+        assign2: 100
+    },
+    Kelly: {
+        assign1: 90,
+        assign2: 90
+    }
+}
+
+
+//Pick and Omit
+
+type AssignResult = Pick<Assignment, "studentId" | "grade">
+
+
+const score: AssignResult = {
+    studentId: '123',
+    grade: 100
+}
+
+type AssignPreview = Omit<Assignment, "grade" | "verified">
+
+const preview: AssignPreview = {
+    studentId: '123',
+    grade: 'Math'
+}
 
 console.log('------------finish fifthe');
+
+
+4: 40
