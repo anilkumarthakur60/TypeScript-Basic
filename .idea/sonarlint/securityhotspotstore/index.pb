c2060a65da20bc6b66670b0e6ff2dca3954b631a
Anil <anilkumarthakur60@gmail.com>
2023-04-07 23:00:34 +0545

    update

--- .idea/sonarlint/securityhotspotstore/index.pb
+++
@@ -1,3 +0,0 @@
-
-;
-src/main.ts,1/a/1af9a5bdf96ddff3a2f3427ed520b7005e9564ad
---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import { useState, useEffect, useCallback, MouseEvent, KeyboardEvent, useMemo, useRef } from "react";
+
+interface User {
+  id: number;
+  username: string;
+}
+function App() {
+  // const  [count, setCount] = useState<User[]|null>(null);
+  // const  [count, setCount] = useState<User>(null);
+  // const  [count, setCount] = useState<User>({} as User);
+  const [count, setCount] = useState<number>(0);
+  const [users, setUsers] = useState<User[] | null>(null);
+
+  useEffect(() => {
+
+    console.log('-----', users)
+    return () => {
+      console.log("unmount");
+    }
+
+  }, [users])
+
+
+  const addTwo = useCallback((e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>): void => {
+
+    setCount((val) => val + 2)
+
+  }, [])
+
+
+  type fibFunc = (n: number) => number
+
+  const fib: fibFunc = (n) => {
+    if (n < 2) {
+      return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+  }
+
+
+  const myNum: number = 37
+  const result: number = useMemo<number>(() => {
+    return fib(myNum)
+  }, [myNum])
+
+
+  // const inputRef = useRef<HTMLInputElement>(null!) //non  null assettion
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  console.log(inputRef?.current)
+  console.log(inputRef?.current?.value)
+
+  return (
+    <div>
+      <h1>React Typescript</h1>
+      <pre>
+        {JSON.stringify(count, null, 2)}
+      </pre>
+      <button onClick={addTwo}>Increment</button>
+
+      <h2>{result}</h2>
+      <button onClick={() => setCount((val) => val + 1)}>Increment</button>
+      <input ref={inputRef} type="text"  />
+    </div>
+  )
+}
+export default App;
