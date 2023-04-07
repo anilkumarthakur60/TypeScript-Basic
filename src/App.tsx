78ed284433b9f2547ddfbba6a0a8b6f90a44e13c
Anil <anilkumarthakur60@gmail.com>
2023-04-08 02:22:26 +0545

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,17 @@
-import { useState, useEffect, useCallback, MouseEvent, KeyboardEvent, useMemo, useRef } from "react";
-import Counter from "./Counter";
-
-import { CounterProvider } from "./context/CounterContext"
-
-interface User {
-    id: number;
-    username: string;
-}
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-    // const  [count, setCount] = useState<User[]|null>(null);
-    // const  [count, setCount] = useState<User>(null);
-    // const  [count, setCount] = useState<User>({} as User);
-    const [count, setCount] = useState<number>(0);
-    const [users, setUsers] = useState<User[] | null>(null);
+  const [count, setCount] = useState(0)
 
-    useEffect(() => {
-
-        console.log('-----', users)
-        return () => {
-            console.log("unmount");
-        }
-
-    }, [users])
+  return (
+    <div className="App">
 
 
-    const addTwo = useCallback((e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>): void => {
-
-        setCount((val) => val + 2)
-
-    }, [])
-
-
-    type fibFunc = (n: number) => number
-
-    const fib: fibFunc = (n) => {
-        if (n < 2) {
-            return n;
-        }
-        return fib(n - 1) + fib(n - 2);
-    }
-
-
-    const myNum: number = 37
-    const result: number = useMemo<number>(() => {
-        return fib(myNum)
-    }, [myNum])
-
-
-    // const inputRef = useRef<HTMLInputElement>(null!) //non  null assettion
-
-    const inputRef = useRef<HTMLInputElement>(null)
-
-    console.log(inputRef?.current)
-    console.log(inputRef?.current?.value)
-
-
-    const currentCount = (num: number) => {
-        return <>
-            Current Count: {num}
-        </>
-    }
-
-
-    return (
-        <div>
-            {/*<button onClick={() => setCount((val) => val + 1)}>Increment</button>*/}
-            {/*<input ref={inputRef} type="text"/>*/}
-            <CounterProvider >
-                <Counter>
-
-                    {currentCount}
-
-                </Counter>
-            </CounterProvider>
-
-        </div>
-    )
+    </div>
+  )
 }
 
-export default App;
+export default App
