5e8116d5e2863838c7bc6ede10eca169ca1e205d
Anil <anilkumarthakur60@gmail.com>
2023-04-09 02:00:51 +0545

    update

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,6 @@
-
-import { ReducerActionType, ReducerAction } from "../context/CartProvider"
-import { ReactElement, memo } from "react"
-import { ProductType } from "../context/ProductProvider"
+import {ReducerAction, ReducerActionType} from "../context/CartProvider"
+import {memo, ReactElement} from "react"
+import {ProductType} from "../context/ProductProvider"
 
 type PropsType = {
     product: ProductType,
@@ -17,14 +16,11 @@ const Product = ({ product, dispatch, REDUCER_ACTIONS, inCart }: PropsType): Rea
 
     const itemInCart = inCart ? ' → Item in Cart: ✔️' : null
 
-    const content =
-        <article className="product">
-            <h3>{product.name}</h3>
-            <p>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(product.price)}{itemInCart}</p>
-            <button onClick={onAddToCart}>Add to Cart</button>
-        </article>
-
-    return content
+    return <article className="product">
+        <h3>{product.name}</h3>
+        <p>{new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(product.price)}{itemInCart}</p>
+        <button onClick={onAddToCart}>Add to Cart</button>
+    </article>
 }
 
 function areProductsEqual({ product: prevProduct, inCart: prevInCart }: PropsType, { product: nextProduct, inCart: nextInCart }: PropsType) {
